646213fd21ec16be486379aa7181ddabfb09cfd7
geniigrace <jeyeom10@naver.com>
2023-01-19 16:44:47 +0900

    오탈자 수정

--- shop 2.x/src/main/java/com/shop/controller/MemberController.java
+++ shop 2.x/src/main/java/com/shop/controller/MemberController.java
@@ -5,7 +5,6 @@ import com.shop.dto.MemberFormDto;
 import com.shop.entity.Member;
 import com.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.Banner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
